cdec893dab383a6654f0999057db48b2daa7d53e
malaybaku <njslyr.trpg+harriet@gmail.com>
2019-11-17 23:06:44 +0900

    add project data

---
+++ TestEmptyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestEmptyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
